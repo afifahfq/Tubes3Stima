a2d5cafc88c1d89e5d2ac0ba4cd6f1e264fd5ee5
13519042 Haning Nanda Hapsari <13519042@std.stei.itb.ac.id>
2021-04-27 22:37:15 +0700

    update, dl task, delete task

--- regex.py
+++ regex.py
@@ -1,10 +1,11 @@
 import re
+from time import process_time
 import mysql.connector
 from datetime import datetime
 
 mysqldb = mysql.connector.connect(host='localhost', user='root',passwd='', database='reminder')
 curs = mysqldb.cursor()
-text = "Halo tolong dicatat ya matkul IF2230 ada tubes materi string matching 05 april 2021"
+#text = "Halo tolong dicatat ya matkul IF2230 ada tucil materi string matching 05 april 2021"
 
 def addTask(tes):
     text = tes.lower()
@@ -74,9 +75,8 @@ def printTask(query):
     elif (query == "today"):
         tgl = datetime.date(datetime.now())
         
-        sql = "SELECT * FROM catatan WHERE jenis = %s"
-        val = "tubes"
-        curs.execute(sql, val)
+        sql = "SELECT * FROM catatan WHERE jenis = %(tubes)s"
+        curs.execute(sql, {'tubes':'tubes'})
         result = curs.fetchall()
 
         for data in result:
@@ -85,5 +85,62 @@ def printTask(query):
         print(type(query))
         print("wrong")
     
+def deleteTask(in_id):
+    sqlid = "SELECT id from catatan where id=%(id)s"
+    curs.execute(sqlid,{'id' : in_id})
+    result = curs.fetchall()
+    if(len(result)!=0):
+        sql = "DELETE FROM catatan WHERE id = %(id)s"
+        curs.execute(sql, {'id':in_id})
+        mysqldb.commit()
+        return True
+    else:
+        return False
+
+def updateTask(in_id, in_tanggal):
+    month = ['januari','februari','maret','april','mei','juni','juli','agustus','september','oktober','november','desember']
+    patt = '[0-9]{2}[/][0-9]{2}[/][0-9]{4}'
+    patt2 = '[0-9]{2}[-][0-9]{2}[-][0-9]{4}'
+    sqlid = "SELECT id from catatan where id=%(id)s"
+    curs.execute(sqlid,{'id' : in_id})
+    result = curs.fetchall()
+    if(len(result)!=0):
+        if(re.search(patt,in_tanggal)):
+            tanggal = str(in_tanggal).split('/')
+            tanggal_fix=(tanggal[2]+'/'+tanggal[1]+'/'+tanggal[0])
+        elif(re.search(patt2,in_tanggal)):
+            tanggal = str(in_tanggal).split('-')
+            tanggal_fix=(tanggal[2]+'/'+tanggal[1]+'/'+tanggal[0])
+        else:
+            tanggal = str(in_tanggal).split()
+            bulan = 0
+            for i in range(12):
+                if(tanggal[1]==month[i]):
+                    bulan=i+1
+                    if(bulan<10):
+                        bulan = '0'+str(bulan)
+                    else:
+                        bulan = str(bulan)
+            tanggal_fix=(tanggal[2]+'/'+bulan+'/'+tanggal[0])
+        sqlupdate = 'UPDATE catatan SET tanggal = %s WHERE id = %s'
+        val = (tanggal_fix,in_id)
+        curs.execute(sqlupdate,val)
+        mysqldb.commit()
+        return True
+    else:
+        return False
+
+def askDeadline(in_matkul):
+    sql = "SELECT * FROM catatan WHERE matkul=%(matkul)s and (jenis='tubes' or jenis='tucil')"
+    curs.execute(sql, {'matkul':in_matkul})
+    result = curs.fetchall()
+
+    for data in result:
+        print(data)
 
 #print(addTask(text))
+# printTask(all)
+#print(deleteTask(1))
+#askDeadline('IF2230')
+
+
